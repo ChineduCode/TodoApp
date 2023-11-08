c8068d3c08d7c6c5e359864c34afb596856491be
Chinedu <chinedunnaemeka2@gmail.com>
2023-11-08 11:53:33 +0100

    auth folder added and updated

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -11,43 +11,31 @@ export async function POST(request){
     }
 
     try {
-        // //connect to database
-        // await connectDatabase()
+        //connect to database
+        await connectDatabase()
 
-        // //transform username to lowercase
-        // username = username.toLowerCase()
+        //transform username to lowercase
+        username = username.toLowerCase()
 
-        // const user = await User.findOne({username})
+        const user = await User.findOne({username})
 
-        // //check if user does not exist
-        // if(!user){
-        //     console.log('User does not exist')
-        //     return new Response(`User does not exist`, {status : 401})
-        // }
-
-        // //check if password is correct
-        // const validPassword = await bcrypt.compare(password, user.password)
-        // if(!validPassword){
-        //     console.log('Incorrect password')
-        //     return new Response('Incorrect password', {status : 401})
-        // }
-
-        // const { password, ...userWithoutPass } = user
-        const user = {
-            id: 2,
-            username: 'chineducode',
-            password: '2020'
+        //check if user does not exist
+        if(!user){
+            console.log('User does not exist')
+            return new Response(`User does not exist`, {status : 401})
         }
 
-        if(username !== user.username){
-            throw new Error('User does not exist')
+        //check if password is correct
+        const validPassword = await bcrypt.compare(password, user.password)
+        if(!validPassword){
+            console.log('Incorrect password')
+            return new Response('Incorrect password', {status : 401})
         }
 
-        if(password !== user.password){
-            throw new Error('Wrong Password')
-        }
+        const {password, ...userWithoutPass} = user
+        console.log(userWithoutPass)
 
-        return new Response(user, {status: 200})
+        return new Response(userWithoutPass, {status: 200})
 
     } catch (error) {
         console.error(error)
