28112bcd18633d7f221784dd1d445ee9fcb333ca
Chinedu <chinedunnaemeka2@gmail.com>
2023-11-11 12:17:32 +0100

    layout modified

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
-'use client'
 import '../scss/globals.css'
 import { Josefin_Sans } from 'next/font/google'
-import { SessionProvider } from 'next-auth/react'
+import Providers from '@/components/Providers'
 
 const josefin_sans = Josefin_Sans({ subsets: ['latin'] })
 
@@ -14,9 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={josefin_sans.className}>
-        <SessionProvider>
+        <Providers>
           {children}
-        </SessionProvider>
+        </Providers>
       </body>
     </html>
   )
