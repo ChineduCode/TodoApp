10813ac54c6f70c24c5f324723f3add450b4cdaf
Chinedu <chinedunnaemeka2@gmail.com>
2023-11-20 01:54:58 +0100

    registerpage modified

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,6 +1,12 @@
 import Register from "@/components/Register";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function RegisterPage(){
+export default async function RegisterPage(){
+    const session = await getServerSession()
+
+    if(session) redirect('/todo')
+
     return( 
         <Register />
     )
