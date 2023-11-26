52745aab75b2601ea447d0712c0ddc2ecc807814
Chinedu <chinedunnaemeka2@gmail.com>
2023-11-26 19:55:16 +0100

    modified app/page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,7 +4,6 @@ import { redirect } from "next/navigation"
 
 export default async function LoginPage(){
     const session = await getServerSession()
-    //console.log(session)
 
     if(session){
         redirect('/todo')
