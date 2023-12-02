d9012014a134142b2f177c667d1daf7321f7d8b5
Chinedu <chinedunnaemeka2@gmail.com>
2023-12-03 00:52:26 +0100

    modified:  components/Todo.jsx

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -91,7 +91,7 @@ export default function Todo(){
                         </div>
                     </section> 
                 }
-
+                
             </section>
         </main>
     )
