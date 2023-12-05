9d38e0b862f41a06ceb16d1a7747ff7689582c8a
Chinedu <chinedunnaemeka2@gmail.com>
2023-12-05 01:23:08 +0100

    modified:   components/CreateNewTodo.jsx

--- components/CreateNewTodo.jsx
+++ components/CreateNewTodo.jsx
@@ -24,8 +24,6 @@ export default function CreateNewTodo(){
 
             if(res.ok){
                 setText('')
-                // router.refresh()
-                // router.push('/todo')
             }
 
         }catch (error) {
