6f018d8cdcfc3db404e4d289db5086e10d535e58
Chinedu <chinedunnaemeka2@gmail.com>
2023-12-07 02:45:30 +0100

    modified:   components/Todo.jsx

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -1,17 +1,8 @@
 import Header from "@/components/Header"
 import CreateNewTodo from "@/components/CreateNewTodo"
 import TodoList from "./TodoList"
-import { getServerSession } from "next-auth"
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 
 export default async function Todo(){
-    // const session = await getServerSession(authOptions)
-    // const apiUrl = process.env.API_URL
-    // if(session){
-    //     const res = await fetch(`${apiUrl}/api/todo`, {})
-    //     const data = await res.json()
-    //     console.log(data)
-    // }
 
     return(
         <main className="todo">
