46d7465fceebd135cf464efd5aa1402485421de2
Chinedu <chinedunnaemeka2@gmail.com>
2023-12-08 23:16:53 +0100

    modified:   components/Todo.jsx

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -12,7 +12,7 @@ export default async function Todo(){
             <section className="container">
                 <Header />
                 <CreateNewTodo />
-                {/* <TodoList /> */}
+                <TodoList />
             </section>
         </main>
     )
