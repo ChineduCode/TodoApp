7d398401c654112e6ba92dd23de03965848e405f
Chinedu <chinedunnaemeka2@gmail.com>
2023-12-11 04:43:38 +0100

    modified:   components/Todo.jsx

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -5,8 +5,6 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 import { getServerSession } from "next-auth"
 
 export default async function Todo(){
-    const session = await getServerSession(authOptions)
-    console.log(session)
     return(
         <main className="todo">
             <section className="container">
